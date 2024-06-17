855eafc1677d3bb0dda3ffd60b594c8278b8b03c
nasser <naserkhanzadii@gmail.com>
2024-06-17 19:27:49 +0330

    1

--- app.php
+++ app.php
@@ -1,10 +1,12 @@
 <?php
-if (isset($_POST)) {
-
+if (isset($_POST['productcode_start'])) {
     $newJsonString = json_encode($_POST);
     file_put_contents('DATA.json', $newJsonString);
-    echo $newJsonString;
+    // echo $newJsonString;
+    header("Location: /");
+    die();
+} elseif (isset($_POST['ExcelData'])) {
+    $newJsonString = json_encode($_POST['ExcelData']);
+    file_put_contents('EXCEL.json', $newJsonString);
+    // echo json_encode($_POST['ExcelData']);
 }
-
-header("Location: /");
-die();
