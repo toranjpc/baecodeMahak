c9ef6c220f01686c212feaeb17fd14609508b4f6
nasser <naserkhanzadii@gmail.com>
2024-10-02 01:07:45 +0330

    0

--- app.php
+++ app.php
@@ -1,12 +1,16 @@
 <?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+
 if (isset($_POST['productcode_start'])) {
+    // $path = exo_getglobalvariable('HEPubStorageLocation', '');
     $newJsonString = json_encode($_POST);
-    file_put_contents('DATA.json', $newJsonString);
+    file_put_contents($path . '\DATA.json', $newJsonString);
     // echo $newJsonString;
     header("Location: /");
     die();
 } elseif (isset($_POST['ExcelData'])) {
+    // $path = exo_getglobalvariable('HEPubStorageLocation', '');
     $newJsonString = json_encode($_POST['ExcelData']);
-    file_put_contents('EXCEL.json', $newJsonString);
+    file_put_contents($path . '\EXCEL.json', $newJsonString);
     // echo json_encode($_POST['ExcelData']);
 }
